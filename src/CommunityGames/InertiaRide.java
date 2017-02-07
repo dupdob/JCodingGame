2d16b53e861cfff9551c7275c99aceb6accaeab4
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2017-02-07 22:54:22 +0100

    InertiaRide: done

--- src/CommunityGames/InertiaRide.java
+++ src/CommunityGames/InertiaRide.java
@@ -1,28 +1,56 @@
 package CommunityGames;
 
-import java.util.*;
-import java.io.*;
-import java.math.*;
+import java.util.Scanner;
 
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-class Solution {
+class InertiaRide {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int inertia = in.nextInt();
         int W = in.nextInt();
         int H = in.nextInt();
+        char[] ride = new char[W];
         in.nextLine();
         for (int i = 0; i < H; i++) {
             String row = in.nextLine();
+            for (int j = 0; j < row.length(); j++) {
+                switch (row.charAt(j)){
+                    case '/':
+                    case '\\':
+                    case '_':
+                        ride[j] = row.charAt(j);
+                        break;
+                }
+            }
         }
-
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-
-        System.out.println("answer");
+        int currentPos=0;
+        while (inertia!=0 || ride[currentPos]!='_'){
+            switch (ride[currentPos]){
+                case '/':
+                    inertia-= inertia>=0 ? 10 : 9;
+                    break;
+                case '\\':
+                    inertia+= inertia>=0 ? 9 : 10;
+                    break;
+                case '_':
+                    inertia-= inertia>0 ? 1 : -1;
+                    break;
+            }
+            if (inertia<0){
+                if (currentPos==0)
+                    break;
+                currentPos--;
+            } else if (inertia>0){
+                if (currentPos==W-1)
+                    break;
+                currentPos++;
+            }
+            System.err.format("%d (%d)%n", currentPos, inertia);
+        }
+        System.out.println(currentPos);
     }
 }
